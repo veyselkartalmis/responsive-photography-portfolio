bdae7156b3bc3d95a3bf581a9b719f99428862e6
Veysel <veyselkartalms@gmail.com>
2022-08-08 21:35:36 +0300

    header style code added

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+// NAVBAR SCROLL
 window.addEventListener("scroll", () => {
-    document.querySelector("nav").classList.toggle("window-scrolled").window.scrollY > 0;
+    document.querySelector("nav").classList.toggle("window-scrolled", window.scrollY > 0);
+});
+
+// CONTACT BUTTONS CIRCULAR TEXT
+const textButtons = document.querySelectorAll(".contact__btn");
+textButtons.forEach(textButton => {
+    let text = textButton.querySelector("p");
+    text.innerHTML = text.innerHTML.split("").map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join("");
 });
