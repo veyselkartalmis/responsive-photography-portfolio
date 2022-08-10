3ff83ca905fdf90a7472be31bc85061c5defa84b
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-08-10 18:49:07 +0300

    gsap code added

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ var swiper = new Swiper(".mySwiper", {
 			slidesPerView: 2,
 			spaceBetween: 40,
 		},
-        1023: {
+		1023: {
 			slidesPerView: 3,
 			spaceBetween: 60,
 		},
@@ -45,25 +45,125 @@ const openBtn = document.querySelector("#nav__toggle-open");
 const closeBtn = document.querySelector("#nav__toggle-close");
 
 const openNav = () => {
-    nav.style.opacity = 1;
-    nav.style.visibility = "visible";
-    nav.style.pointerEvents = "visible";
-    openBtn.style.display = "none";
-    closeBtn.style.display = "inline-block";
-}
+	nav.style.opacity = 1;
+	nav.style.visibility = "visible";
+	nav.style.pointerEvents = "visible";
+	openBtn.style.display = "none";
+	closeBtn.style.display = "inline-block";
+};
 
 openBtn.addEventListener("click", openNav);
 
 const closeNav = () => {
-    nav.style.opacity = 0;
-    nav.style.visibility = "hidden";
-    closeBtn.style.display = "none";
-    openBtn.style.display = "inline-block";
-}
+	nav.style.opacity = 0;
+	nav.style.visibility = "hidden";
+	closeBtn.style.display = "none";
+	openBtn.style.display = "inline-block";
+};
 
 closeBtn.addEventListener("click", closeNav);
 
-nav.querySelectorAll("li a").forEach(navLink => {
-    navLink.addEventListener("click", closeNav);
+if (document.body.clientWidth < 1024) {
+	nav.querySelectorAll("li a").forEach((navLink) => {
+		navLink.addEventListener("click", closeNav);
+	});
+}
+
+/* GSAP ANIMATION */
+gsap.to("header .empty", {
+    width: "18rem",
+    duration: 1.5,
+	ease: "none",
 });
 
+gsap.to(".header__right h1", {
+	opacity: 1,
+	transform: "translateY(0)",
+	ease: "none",
+});
+
+gsap.to(".header__right p", {
+	opacity: 1,
+	transform: "translateY(0)",
+	delay: 0.2,
+	ease: "none",
+});
+
+gsap.to(".about__image-sm", {
+	y: 75,
+	ease: "none",
+	scrollTrigger: {
+		trigger: "#about",
+		start: "top center",
+		end: "bottom+=200 center",
+		scrub: true,
+	},
+});
+
+gsap.to(".about__image-lg", {
+	y: -75,
+	ease: "none",
+	scrollTrigger: {
+		trigger: "#about",
+		start: "top center",
+		end: "bottom+=200 center",
+		scrub: true,
+	},
+});
+
+gsap.to("#about .empty", {
+	width: "18rem",
+	ease: "none",
+	scrollTrigger: {
+		trigger: "#about",
+		start: "top-=400 center",
+		end: "bottom+=-100 center",
+		scrub: true,
+	},
+});
+
+gsap.to("#gallery .empty", {
+	width: "18rem",
+	ease: "none",
+	scrollTrigger: {
+		trigger: "#gallery",
+		start: "top-=400 center",
+		end: "bottom+=-500 center",
+		scrub: true,
+	},
+});
+
+gsap.to(".swiper-slide", {
+    translateY: "0",
+    opacity: 1,
+	ease: "none",
+	scrollTrigger: {
+		trigger: "#gallery",
+		start: "top-=50 center",
+		end: "bottom+=-100 center",
+	},
+    stagger: .1,
+});
+
+gsap.to("#exhibitions .empty", {
+    width: "18rem",
+	ease: "none",
+	scrollTrigger: {
+		trigger: "#exhibitions",
+		start: "top-=400 center",
+		end: "top+=200 center",
+        scrub: true,
+	},
+});
+
+gsap.to(".exhibitions__gallery article", {
+    translateY: 0,
+    opacity: 1,
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#exhibitions",
+        start: "top center",
+        end: "bottom-=100 center",
+    },
+    stagger: .2,
+});    
