a464ba156ac797aef4ec08d6c4cda0c01de0f25f
Veysel Kartalmis <veyselkartalms@gmail.com>
2022-08-10 11:13:47 +0300

    gallery section & swiper added

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
-// NAVBAR SCROLL
+/* NAVBAR SCROLL */
 window.addEventListener("scroll", () => {
-    document.querySelector("nav").classList.toggle("window-scrolled", window.scrollY > 0);
+	document
+		.querySelector("nav")
+		.classList.toggle("window-scrolled", window.scrollY > 0);
 });
 
-// CONTACT BUTTONS CIRCULAR TEXT
+/* CONTACT BUTTONS CIRCULAR TEXT */
 const textButtons = document.querySelectorAll(".contact__btn");
-textButtons.forEach(textButton => {
-    let text = textButton.querySelector("p");
-    text.innerHTML = text.innerHTML.split("").map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join("");
+textButtons.forEach((textButton) => {
+	let text = textButton.querySelector("p");
+	text.innerHTML = text.innerHTML
+		.split("")
+		.map(
+			(character, index) =>
+				`<span style="transform: rotate(${index * 12}deg)">${character}</span>`
+		)
+		.join("");
+});
+
+/* SWIPER */
+var swiper = new Swiper(".mySwiper", {
+	slidesPerView: 1,
+	speed: 900,
+	spaceBetween: 20,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	breakpoints: {
+		599: {
+			slidesPerView: 2,
+			spaceBetween: 40,
+		},
+        1023: {
+			slidesPerView: 3,
+			spaceBetween: 60,
+		},
+	},
 });
